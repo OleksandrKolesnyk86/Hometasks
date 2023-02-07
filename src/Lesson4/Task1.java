bf1a2441579d33034d6795272dd6e3b59b708df6
OleksandrKolesnyk86 <sanyakolesnik1986@gmail.com>
2023-02-07 22:10:55 +0100

    Hometasks from Lesson4

---
+++ src/Lesson4/Task1.java
@@ -0,0 +1,12 @@
+package Lesson4;
+import java.util.Arrays;
+public class Task1 {
+     public static void main(String[] args) {
+        int[] numbers = {9, 10, 3, 4, 5};
+        Arrays.sort(numbers);
+        System.out.println(numbers[0]);
+        }
+    }
+//another way
+
+/*1. Написать программу, которая находит минимальное значение в массиве*/
