bfbd2bc099458a4d5e68d5794f933d684f14518d
OleksandrKolesnyk86 <sanyakolesnik1986@gmail.com>
2023-02-08 21:28:36 +0100

    Hometasks from Lesson4

--- src/Lesson4/Task1.java
+++ src/Lesson4/Task1.java
@@ -1,12 +1,28 @@
 package Lesson4;
-import java.util.Arrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
-     public static void main(String[] args) {
-        int[] numbers = {9, 10, 3, 4, 5};
-        Arrays.sort(numbers);
-        System.out.println(numbers[0]);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите количество элементов массива");
+        int size = scan.nextInt();
+        int[] numbers = new int[size];
+        Random rnd = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rnd.nextInt(300);
+            System.out.print(numbers[i] + " ");
         }
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (min > numbers[i]) {
+                min = numbers[i];
+            }
+        }
+        System.out.println("\nМинимальное значение массива = " + min);
     }
+}
 //another way
 
 /*1. Написать программу, которая находит минимальное значение в массиве*/
