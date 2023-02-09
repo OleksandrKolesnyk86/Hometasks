ba621436d8c311607d89bffede327487d6abefd1
OleksandrKolesnyk86 <sanyakolesnik1986@gmail.com>
2023-02-09 19:23:36 +0100

    Hometasks from Lesson4

---
+++ src/Lesson4/Task3.java
@@ -0,0 +1,17 @@
+package Lesson4;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task3 {
+    public static void main (String[] args){
+        int[] array = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i <= array.length-1; i++){
+            array[i]= rnd.nextInt(20);
+            System.out.println(array[i]);
+        }
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
+/*3. Написать программу, которая сортирует массив от минимума в максимум*/
