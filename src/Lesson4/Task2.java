34c1a5e0c09dde61fb0337f7e8093511202c56f6
OleksandrKolesnyk86 <sanyakolesnik1986@gmail.com>
2023-02-10 21:36:46 +0100

    Hometasks from Lesson4

--- src/Lesson4/Task2.java
+++ src/Lesson4/Task2.java
@@ -2,15 +2,20 @@ package Lesson4;
 
 public class Task2 {
     public static void main(String[] args) {
-        int[] degree = new int[360];
+        int i = 10;
+        while (i<=360){
+            System.out.println("Синус " + i + " градусов = " + Math.sin(i));
+            i = i + 10;
+        }
+    }
+}
+/*int[] degree = new int[360];
         for (int i = 1; i <= degree.length - 1; i++) {
             degree[i] = i;
         }
         for (int a = 10; a <= degree.length + 1; a = a + 10) {
             System.out.println("Синус " + a + " градусов = " + Math.sin(a));
-        }
-    }
-}
+        }*/
 /*2. Написать программу, которая печатает значения sin(x), где x это каждые 10 градусов
 от 0 до 360.
 */
