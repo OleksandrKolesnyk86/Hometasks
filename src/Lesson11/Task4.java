903d3bcfcbb2b81894693028d968fda138dc5c75
OleksandrKolesnyk86 <sanyakolesnik1986@gmail.com>
2023-03-07 20:10:14 +0100

    HomeTasksLesson11

---
+++ src/Lesson11/Task4.java
@@ -0,0 +1,19 @@
+package Lesson11;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String pet = "cat";
+        System.out.println(backAround(pet));
+    }
+
+    public static String backAround(String str) {
+        String a = str.substring(str.length()-1);
+        return a + str + a;
+    }
+}
+/*4. Взять последний символ в строке и вернуть новую строку с последним
+символом, добавленным спереди и сзади, «cat» -> «tcatt».
+public String backAround(String str) {}
+"cat" → "tcatt"
+"Hello" → "oHelloo"
+"a" → "aaa"*/
