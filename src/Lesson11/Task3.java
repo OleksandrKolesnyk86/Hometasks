9b0175b93c5987e5596dc4afc19fe02d9cd7f899
OleksandrKolesnyk86 <sanyakolesnik1986@gmail.com>
2023-03-07 19:53:29 +0100

    HomeTasksLesson11

--- src/Lesson11/Task3.java
+++ src/Lesson11/Task3.java
@@ -7,11 +7,11 @@ public class Task3 {
     }
 
     public static String repeatRepeatRepeat(String inputString) {
-        String a = inputString.substring(0,3);
-        String newString = a+a+a;
-        if (inputString.length()>3){
+        String a = inputString.substring(0, 3);
+        String newString = a + a + a;
+        if (inputString.length() > 3) {
             return newString;
-        }else {
+        } else {
             return inputString;
         }
     }
